80b31dc86e389f62da914d9b2a84bfa8075c8e00
Jesse Otim <otimdanieljesse@gmail.com>
2023-10-09 11:22:20 +0300

    Add new files and refactor code

--- apps/routes/routes.js
+++ apps/routes/routes.js
@@ -1,11 +1,9 @@
-import express  from 'express';
-import AuthController from '../controllers/authController';
-
+import express from "express";
+import AuthController from "../controllers/authController";
 
 const router = express.Router();
 
-router.post('/api/auth/signup', AuthController.signup);
-router.post('/api/auth/login', AuthController.login);
+router.post("/api/auth/signup", AuthController.signup);
+router.post("/api/auth/login", AuthController.login);
 
 export default router;
-
